9c52efdb063cee168e2ff35f276511e6a643f4e6
Sam <sam.igknighted@gmail.com>
2016-01-18 06:18:16 +0000

    FTP chroot fix

--- dev_run.sh
+++ dev_run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-go get -u
-. pack_assets.sh
-go run main.go
+if [ "$1" == "first" ]; then
+	go get -u
+	. pack_assets.sh
+	cp settings.example.cfg settings.cfg
+	go run main.go /api/install
+else
+	go get -u
+	. pack_assets.sh
+	go run main.go
+fi
