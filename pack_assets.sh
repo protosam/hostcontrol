85cfc4c42cff1f02333dbafd8dcd525910d1dabb
Sam <samigknighted@gmail.com>
2017-04-21 16:56:11 -0500

    Switched to a fork for go-bindata, OC didn't maintain it

--- pack_assets.sh
+++ pack_assets.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-MYSQLCHECK=$(mysqldump --no-data --compact --skip-comments hostcontrol | grep -v '^\/\*![0-9]\{5\}.*\/;$' | wc -l)
-if [ "$MYSQLCHECK" != "0" ]; then
-    echo Making an up-to-date MySQL dump
-    mysqldump --no-data hostcontrol > common/hostcontrol.sql
+
+if which mysqldump > /dev/null 2>&1; then
+	MYSQLCHECK=$(mysqldump --no-data --compact --skip-comments hostcontrol | grep -v '^\/\*![0-9]\{5\}.*\/;$' | wc -l)
+	if [ "$MYSQLCHECK" != "0" ]; then
+	    echo Making an up-to-date MySQL dump
+	    mysqldump --no-data hostcontrol > common/hostcontrol.sql
+	fi
 fi
+
+# jteeuwen hasn't updated go-bindata since golang moved vet out of experimental
+# seanmcgary made some maintenance changes
 
 if ! which $GOPATH/bin/go-bindata; then
 	echo Installing go-bindata;
-	go get -u github.com/jteeuwen/go-bindata
-	cd $GOPATH/src/github.com/jteeuwen/go-bindata
+	go get -u github.com/seanmcgary/go-bindata
+	cd $GOPATH/src/github.com/seanmcgary/go-bindata
 	pwd
 	# try make...
 	if ! make; then
